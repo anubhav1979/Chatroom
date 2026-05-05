a5c5254297922d75f542a5027fb09f343fb40868
Anubhav <anubhavsinghrathore03@gmail.com>
2026-05-06 03:54:21 +0530

    fix cors for production

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,7 @@ const server = http.createServer(app);
 
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://your-app.vercel.app"
+  "https://chatroom-4d1r.vercel.app"
 ];
 
 app.use(cors({
